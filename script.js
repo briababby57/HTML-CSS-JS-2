b5ef4fd4e4490e396ea922924d88f1ae3c64d0d7
briababby57 <96610248+briababby57@users.noreply.github.com>
2023-09-20 15:57:02 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,132 @@
+Pysnippets is a an online snippet sharing service. It is written in Node.js and uses MongoDB as a database.
+	
+## Installation
+	
+1. Clone the repository
+2. Install dependencies
+3. Start the server
+	
+## Usage
+	
+### Creating a snippet
+	
+1. Navigate to the home page
+2. Click the "Create Snippet" button
+3. Enter a title and description
+4. Click the "Save" button
+5. Copy the snippet ID
+6. Navigate to the snippet page
+7. Click the "Copy" button
+8. Paste the snippet ID into the URL bar
+9. Click the "Copy" button
+10. Share the snippet with your friends
+	
+### Sharing a snippet
+	
+1. Navigate to the home page
+2. Click the "Share Snippet" button
+3. Enter the snippet ID
+4. Click the "Share" button
+5. Share the snippet with your friends
+	
+### Editing a snippet
+	
+1. Navigate to the home page
+2. Click the "Edit Snippet" button
+3. Enter the snippet ID
+4. Click the "Edit" button
+5. Edit the snippet
+6. Click the "Save" button
+7. Share the edited snippet with your friends
+	
+### Deleting a snippet
+	
+1. Navigate to the home page
+2. Click the "Delete Snippet" button
+3. Enter the snippet ID
+4. Click the "Delete" button
+5. Delete the snippet
+6. Navigate to the home page
+7. Click the "Create Snippet" button
+8. Enter a title and description
+9. Click the "Save" button
+10. Copy the snippet ID
+11. Navigate to the snippet page
+12. Click the "Copy" button
+13. Paste the snippet ID into the URL bar
+14. Click the "Copy" button
+15. Share the snippet with your friends
+	
+### Sharing a snippet with a link
+	
+1. Navigate to the home page
+2. Click the "Share Snippet" button
+3. Enter the snippet ID
+4. Click the "Share" button
+5. Share the snippet with your friends
+6. Click the "Share Link" button
+7. Copy the snippet ID
+8. Navigate to the snippet page
+9. Click the "Copy" button
+10. Paste the snippet ID into the URL bar
+11. Click the "Copy" button
+12. Share the snippet with your friends
+	
+### Editing a snippet with a link
+	
+1. Navigate to the home page
+2. Click the "Edit Snippet" button
+3. Enter the snippet ID
+4. Click the "Edit" button
+5. Edit the snippet
+6. Click the "Save" button
+7. Share the edited snippet with your friends
+8. Click the "Share Link" button
+9. Copy the snippet ID
+10. Navigate to the snippet page
+11. Click the "Copy" button
+12. Paste the snippet ID into the URL bar
+13. Click the "Copy" button
+14. Share the snippet with your friends
+	
+### Deleting a snippet with a link
+	
+1. Navigate to the home page
+2. Click the "Delete Snippet" button
+3. Enter the snippet ID
+4. Click the "Delete" button
+5. Delete the snippet
+6. Navigate to the home page
+7. Click the "Create Snippet" button
+8. Enter a title and description
+9. Click the "Save" button
+10. Copy the snippet ID
+11. Navigate to the snippet page
+12. Click the "Copy" button
+13. Paste the snippet ID into the URL bar
+14. Click the "Copy" button
+15. Share the snippet with your friends
+16. Click the "Share Link" button
+17. Copy the snippet ID
+18. Navigate to the snippet page
+19. Click the "Copy" button
+20. Paste the snippet ID into the URL bar
+21. Click the "Copy" button
+22. Share the snippet with your friends
+	
+### Sharing a snippet with a link and edit
+	
+1. Navigate to the home page
+2. Click the "Share Snippet" button
+3. Enter the snippet ID
+4. Click the "Share" button
+5. Share the snippet with your friends
+6. Click the "Share Link" button
+7. Copy the snippet ID
+8. Navigate to the snippet page
+9. Click the "Copy" button
+10. Paste the snippet ID into the URL bar
+11. Click the "Copy" button
+12. Share the snippet with your friends
+13. Navigate to the snippet page
+14. Click
